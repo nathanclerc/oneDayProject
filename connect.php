9ee9e33ed41770505c3f9ad3c4c73bfb69d07d3c
nathanclerc <39793122+nathanclerc@users.noreply.github.com>
2018-09-05 16:57:06 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
     try {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'simoccauch19','azerty', $pdo_options);
+        $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'x','x', $pdo_options);
     }
     catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage() . "<br />";
